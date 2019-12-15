90481a076e48b367c80fed1dd530f988554ecf1e
lex <lex@mapha.org>
2019-12-16 02:57:24 +0300

    Проба 0.0.8. Попробуем 2 лабу

--- lab2-ip_filter/build/CMakeFiles/ip_filter_lib.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libip_filter_lib.a"
-)
--- lab2-ip_filter/build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lex/Документы/otus-cpp/lab2/my/test
-# Build directory: /home/lex/Документы/otus-cpp/lab2/my/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Name "boostTest" "COMMAND" "boostTest")
